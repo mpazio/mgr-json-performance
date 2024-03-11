dabc9c54684fe376f270c7cc22cf6a17e5fe7f0b
Michał Pazio <elmiciu@gmail.com>
2024-03-11 11:57:21 +0100

    Add Postgres support

---
+++ JSONPerformance/JSONPerformance.Tests/Databases/PostgresTest.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using JSONPerformance.Databases;
+using Xunit;
+
+namespace JSONPerformance.Tests.Databases;
+
+[TestSubject(typeof(JSONPerformance.Databases.Postgres))]
+public class PostgresTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
---
+++ JSONPerformance/JSONPerformance/Databases/Postgres.cs
@@ -0,0 +1,35 @@
+﻿using Npgsql;
+
+namespace JSONPerformance.Databases;
+
+public class Postgres : Database
+{
+    private readonly NpgsqlDataSource _dataSource;
+    private NpgsqlConnection? _connection;
+    
+    public Postgres(string connectionString) : base(connectionString)
+    {
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+        _dataSource = dataSourceBuilder.Build();
+    }
+    
+    public override Task Connect()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<bool> IsConnected()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task SeedDatabase(string[] data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task ExecuteQuery(string query)
+    {
+        throw new NotImplementedException();
+    }
+}
