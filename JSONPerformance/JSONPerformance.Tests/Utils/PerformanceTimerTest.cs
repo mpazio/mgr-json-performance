ff1c98779e667ddc73a588e15df85628db6ccc3b
Michał Pazio <elmiciu@gmail.com>
2024-03-11 11:10:58 +0100

    Add PerformanceTimer method with related tests

---
+++ JSONPerformance/JSONPerformance.Tests/Utils/PerformanceTimerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using JSONPerformance.Utils;
+using Xunit;
+
+namespace JSONPerformance.Tests.Utils;
+
+[TestSubject(typeof(PerformanceTimer))]
+public class PerformanceTimerTest
+{
+
+    [Fact]
+    public async Task ReturnTrueIfTimeMeasureIsWithinErrorMargin()
+    {
+        // Arrange
+        var errorMargin = TimeSpan.FromMilliseconds(100);
+        var timeSpanWithErrorMargin = TimeSpan.FromSeconds(1).Add(errorMargin);
+        
+        // Act
+        var res = await PerformanceTimer.Measure(WaitOneSecond, "someMessage");
+
+        // Assert
+        Assert.True(res < timeSpanWithErrorMargin);
+    }
+
+    private Task WaitOneSecond(string parameter)
+    {
+        Thread.Sleep(1000);
+        return Task.CompletedTask;
+    }
+}
---
+++ JSONPerformance/JSONPerformance/Utils/PerformanceTimer.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace JSONPerformance.Utils;
+
+public static class PerformanceTimer
+{
+    public static async Task<TimeSpan> Measure(Func<string, Task> function, string parameter)
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+        await function(parameter);
+        stopWatch.Stop();
+        TimeSpan ts = stopWatch.Elapsed;
+
+        return ts;
+    }
+}
