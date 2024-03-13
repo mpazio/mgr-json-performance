2098b3f86ca02607bb14331b33852ff8114dca78
Michał Pazio <elmiciu@gmail.com>
2024-03-13 14:39:39 +0100

    Implement methods in Postgres.cs

--- JSONPerformance/JSONPerformance/Databases/Postgres.cs
+++ JSONPerformance/JSONPerformance/Databases/Postgres.cs
@@ -1,35 +1,61 @@
-﻿using Npgsql;
+﻿using System.Data;
+using Npgsql;
 
 namespace JSONPerformance.Databases;
 
 public class Postgres : Database
 {
     private readonly NpgsqlDataSource _dataSource;
-    private NpgsqlConnection? _connection;
-    
+    public NpgsqlConnection? Connection { get; private set; }
+
     public Postgres(string connectionString) : base(connectionString)
     {
         var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
         _dataSource = dataSourceBuilder.Build();
     }
     
-    public override Task Connect()
+    public override async Task Connect()
     {
-        throw new NotImplementedException();
+        Connection = await _dataSource.OpenConnectionAsync();
     }
 
     public override Task<bool> IsConnected()
     {
-        throw new NotImplementedException();
+        if (Connection is null) return Task.FromResult(false);
+        switch (Connection.State)
+        {
+            case ConnectionState.Closed:
+            case ConnectionState.Broken:
+                return Task.FromResult(false);
+            default:
+                return Task.FromResult(true);
+        }
     }
 
-    public override Task SeedDatabase(string[] data)
+    public override async Task SeedDatabase(string[] data)
     {
-        throw new NotImplementedException();
+        if (!await IsConnected()) return;
+        await using (var cmd = new NpgsqlCommand())
+        {
+            cmd.Connection = Connection;
+            foreach (var query in data)
+            {
+                cmd.CommandText = query;
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+        
     }
 
-    public override Task ExecuteQuery(string query)
+    public override async Task ExecuteQuery(string query)
     {
-        throw new NotImplementedException();
+        if (!await IsConnected()) return;
+        await using (var cmd = new NpgsqlCommand())
+        {
+            cmd.Connection = Connection;
+            cmd.CommandText = query;
+
+            await cmd.ExecuteNonQueryAsync();
+        }
     }
 }
