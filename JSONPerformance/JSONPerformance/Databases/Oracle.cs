a19cee1d4abd9c16302c705c405b2e626fbad98c
Michał Pazio <elmiciu@gmail.com>
2024-03-13 15:44:46 +0100

    Change Oracle provider and implement methods for Oracle

--- JSONPerformance/JSONPerformance/Databases/Oracle.cs
+++ JSONPerformance/JSONPerformance/Databases/Oracle.cs
@@ -1,10 +1,11 @@
-﻿using Oracle.ManagedDataAccess.Client;
+﻿using System.Data;
+using Oracle.ManagedDataAccess.Client;
 
 namespace JSONPerformance.Databases;
 
 public class Oracle : Database
 {
-    private OracleConnection _connection;
+    private OracleConnection? _connection;
     
     public Oracle(string connectionString) : base(connectionString)
     {
@@ -18,16 +19,40 @@ public class Oracle : Database
 
     public override Task<bool> IsConnected()
     {
-        throw new NotImplementedException();
+        if (_connection is null) return Task.FromResult(false);
+        switch (_connection.State)
+        {
+            case ConnectionState.Closed:
+            case ConnectionState.Broken:
+                return Task.FromResult(false);
+            default:
+                return Task.FromResult(true);
+        }
     }
 
-    public override Task SeedDatabase(string[] data, params string[]? parameters)
+    public override async Task SeedDatabase(string[] data, params string[]? parameters)
     {
-        throw new NotImplementedException();
+        if (!await IsConnected()) return;
+        await using (var cmd = new OracleCommand())
+        {
+            cmd.Connection = _connection;
+            foreach (var query in data)
+            {
+                cmd.CommandText = query;
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
     }
 
-    public override Task ExecuteQuery(string query)
+    public override async Task ExecuteQuery(string query)
     {
-        throw new NotImplementedException();
+        if (!await IsConnected()) return;
+        await using (var cmd = new OracleCommand())
+        {
+            cmd.Connection = _connection;
+            cmd.CommandText = query;
+
+            await cmd.ExecuteNonQueryAsync();
+        }
     }
 }
