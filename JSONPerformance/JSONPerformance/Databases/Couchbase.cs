d38be90f095613192c830ca6d02ee510b78b64f6
Michał Pazio <elmiciu@gmail.com>
2024-03-13 20:02:32 +0100

    Add Couchbase

---
+++ JSONPerformance/JSONPerformance/Databases/Couchbase.cs
@@ -0,0 +1,47 @@
+﻿using Couchbase;
+
+namespace JSONPerformance.Databases;
+
+public class Couchbase : Database
+{
+    private ICluster _cluster;
+    public Couchbase(string connectionString) : base(connectionString)
+    {
+    }
+
+    public override async Task Connect()
+    {
+        _cluster = await Cluster.ConnectAsync(ConnectionString, new ClusterOptions
+        {
+            UserName = "Administrator",
+            Password = "password",
+        });
+    }
+
+    public override async Task<bool> IsConnected()
+    {
+        try
+        {
+            var bucket = await _cluster.Buckets.GetBucketAsync("data");
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public override Task SeedDatabase(string[] data, params string[]? parameters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task ExecuteQuery(string query)
+    {
+        var statement = "SELECT * FROM `data`";
+
+        var results = await _cluster.QueryAsync<dynamic>(statement);
+
+        await foreach (var result in results) Console.WriteLine(result);
+    }
+}
