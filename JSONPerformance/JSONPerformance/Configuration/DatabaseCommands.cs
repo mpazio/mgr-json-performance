409192a6e9d3cf55d6f2dc0e3ccd94ae7f21b6ca
Michał Pazio <elmiciu@gmail.com>
2024-03-14 20:26:46 +0100

    Add TruncateAndSeed method and implement Seed

--- JSONPerformance/JSONPerformance/Configuration/DatabaseCommands.cs
+++ JSONPerformance/JSONPerformance/Configuration/DatabaseCommands.cs
@@ -12,10 +12,30 @@ public class DatabaseCommands
         var database = CommandsHelpers.GetDatabases(databases, connectionString);
         await database.Connect();
         await database.Truncate(tableName);
+        
+        Console.WriteLine($"Data collection - {tableName} was successfully truncated in database - {databases.ToString()}");
     }
     
-    public void Seed(PossibleDatabases databases, string connectionString, string pathToData)
+    public async Task Seed(PossibleDatabases databases, string connectionString, string pathToData)
     {
+        var database = CommandsHelpers.GetDatabases(databases, connectionString);
+        await database.Connect();
+        var data = await File.ReadAllLinesAsync(pathToData);
+        await database.SeedDatabase(data);
         
+        Console.WriteLine($"Database - {databases.ToString()} - was successfully seeded.");
+    }
+
+    public async Task TruncateAndSeed(PossibleDatabases databases, string connectionString, string tableName, string pathToData)
+    {
+        var database = CommandsHelpers.GetDatabases(databases, connectionString);
+        await database.Connect();
+        await database.Truncate(tableName);
+        Console.WriteLine($"Data collection - {tableName} was successfully truncated in database - {databases.ToString()}");
+        
+        var data = await File.ReadAllLinesAsync(pathToData);
+        await database.SeedDatabase(data);
+        
+        Console.WriteLine($"Database - {databases.ToString()} - was successfully seeded.");
     }
 }
