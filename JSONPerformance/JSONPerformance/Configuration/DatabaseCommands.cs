5c8568709d125a86382cc58485466d1a73d4a4ef
Michał Pazio <elmiciu@gmail.com>
2024-03-18 17:06:29 +0100

    Add parameters for Seed commands

--- JSONPerformance/JSONPerformance/Configuration/DatabaseCommands.cs
+++ JSONPerformance/JSONPerformance/Configuration/DatabaseCommands.cs
@@ -18,19 +18,20 @@ public class DatabaseCommands
         Console.WriteLine($"Data collection - {tableName} was successfully truncated in database - {databases.ToString()}");
     }
     
-    public async Task Seed(PossibleDatabases databases, string connectionString, string pathToData)
+    
+    public async Task Seed(PossibleDatabases databases, string connectionString, string pathToData, [Option('p', "param")] params string[]? additionalParameters)
     {
         var database = CommandsHelpers.GetDatabases(databases, connectionString);
         await database.Connect();
         if (!await database.IsConnected())
             throw new Exception("Not connected to database");
         var data = await File.ReadAllLinesAsync(pathToData);
-        await database.SeedDatabase(data);
+        await database.SeedDatabase(data, additionalParameters);
         
         Console.WriteLine($"Database - {databases.ToString()} - was successfully seeded.");
     }
 
-    public async Task TruncateAndSeed(PossibleDatabases databases, string connectionString, string tableName, string pathToData)
+    public async Task TruncateAndSeed(PossibleDatabases databases, string connectionString, string tableName, string pathToData, [Option('p', "param")] params string[]? additionalParameters)
     {
         var database = CommandsHelpers.GetDatabases(databases, connectionString);
         await database.Connect();
@@ -40,7 +41,7 @@ public class DatabaseCommands
         Console.WriteLine($"Data collection - {tableName} was successfully truncated in database - {databases.ToString()}");
         
         var data = await File.ReadAllLinesAsync(pathToData);
-        await database.SeedDatabase(data);
+        await database.SeedDatabase(data, additionalParameters);
         
         Console.WriteLine($"Database - {databases.ToString()} - was successfully seeded.");
     }
