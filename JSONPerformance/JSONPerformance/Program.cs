048d2ff2abd66557bf40dcd99b72371ba95cf778
Michał Pazio <elmiciu@gmail.com>
2024-03-25 15:39:12 +0100

    Add CommandDotNet to Program.cs

--- JSONPerformance/JSONPerformance/Program.cs
+++ JSONPerformance/JSONPerformance/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CommandDotNet;
+using JSONPerformance.Configuration;
+
+return new AppRunner<Commands>().Run(args);
