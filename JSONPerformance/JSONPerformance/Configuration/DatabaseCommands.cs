80bbe3a5c2019c1eb90c744c0d1d35f2c2ade8f9
Michał Pazio <elmiciu@gmail.com>
2024-03-25 15:38:58 +0100

    Implement CouchDb methods

--- JSONPerformance/JSONPerformance/Configuration/DatabaseCommands.cs
+++ JSONPerformance/JSONPerformance/Configuration/DatabaseCommands.cs
@@ -7,13 +7,13 @@ namespace JSONPerformance.Configuration;
 [Subcommand]
 public class DatabaseCommands
 {
-    public async Task Truncate(PossibleDatabases databases, string connectionString, string tableName)
+    public async Task Truncate(PossibleDatabases databases, string connectionString, string tableName, [Option('p', "param")] params string[]? additionalParameters)
     {
         var database = CommandsHelpers.GetDatabases(databases, connectionString);
         await database.Connect();
         if (!await database.IsConnected())
             throw new Exception("Not connected to database");
-        await database.Truncate(tableName);
+        await database.Truncate(tableName, additionalParameters);
         
         Console.WriteLine($"Data collection - {tableName} was successfully truncated in database - {databases.ToString()}");
     }
--- JSONPerformance/JSONPerformance/Databases/CouchDb.cs
+++ JSONPerformance/JSONPerformance/Databases/CouchDb.cs
@@ -1,5 +1,11 @@
-﻿using CouchDB.Client;
-using CouchDB.Client.FluentMango;
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using CouchDB.Client;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace JSONPerformance.Databases;
 
@@ -11,43 +17,96 @@ public class CouchDb : Database
     public CouchDb(string connectionString) : base(connectionString)
     {
         _httpClient = new HttpClient();
+        Uri baseUri = new Uri(connectionString);
+        _httpClient.BaseAddress = baseUri;
+        _httpClient.DefaultRequestHeaders.Clear();
+        _httpClient.DefaultRequestHeaders.ConnectionClose = true;
     }
 
-    public override async Task Connect()
+    public override Task Connect()
     {
-        _client = new CouchClient(ConnectionString);
+        return Task.CompletedTask;
     }
 
     public override async Task<bool> IsConnected()
     {
-        try
-        {
-            await _client.ListAllDatabasesAsync();
+        HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(ConnectionString);
+        request.AllowAutoRedirect = false;
+        request.Method = "HEAD";
+        try {
+            request.GetResponse();
             return true;
-        }
-        catch (Exception)
+        } catch (WebException wex)
         {
             return false;
         }
     }
 
-    public override Task SeedDatabase(string[] data, params string[]? parameters)
+    public override async Task SeedDatabase(string[] data, params string[]? parameters)
     {
-        throw new NotImplementedException();
+        var base64EncodedAuthenticationString = GetAuthenticationStringInBase64(parameters);
+        var serializedData = ParseData(data);
+        try
+        {
+            _httpClient.DefaultRequestHeaders
+                .Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, "/test/_bulk_docs");
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Basic", base64EncodedAuthenticationString);
+            requestMessage.Content = new StringContent(serializedData,
+                Encoding.UTF8, 
+                "application/json");
+            
+            var response = await _httpClient.SendAsync(requestMessage);
+            response.EnsureSuccessStatusCode();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 
-    public override Task Truncate(string tableName, params string[]? parameters)
+    public override async Task Truncate(string tableName, params string[]? parameters)
     {
-        throw new NotImplementedException();
+        var base64EncodedAuthenticationString = GetAuthenticationStringInBase64(parameters);
+        try
+        {
+            var deleteMessage = new HttpRequestMessage(HttpMethod.Delete, "/test");
+            deleteMessage.Headers.Authorization = new AuthenticationHeaderValue("Basic", base64EncodedAuthenticationString);
+            var deleteResponse = await _httpClient.SendAsync(deleteMessage);
+            deleteResponse.EnsureSuccessStatusCode();
+            
+            var putMessage = new HttpRequestMessage(HttpMethod.Put, "/test");
+            putMessage.Headers.Authorization = new AuthenticationHeaderValue("Basic", base64EncodedAuthenticationString);
+            var putResponse = await _httpClient.SendAsync(putMessage);
+            putResponse.EnsureSuccessStatusCode();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 
     public override async Task ExecuteQuery(string query)
     {
-        var db = await _client.GetDatabaseAsync("test");
+        var base64EncodedAuthenticationString = GetAuthenticationStringInBase64(new string[]{"admin", "password"});
         try
         {
-            await _httpClient.PostAsync($"{ConnectionString}/test/_find",
-                new StringContent(query));
+            _httpClient.DefaultRequestHeaders
+                .Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, "/test/_find");
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Basic", base64EncodedAuthenticationString);
+            requestMessage.Content = new StringContent(query,
+                Encoding.UTF8, 
+                "application/json");
+            
+            var response = await _httpClient.SendAsync(requestMessage);
+            response.EnsureSuccessStatusCode();
         }
         catch (Exception e)
         {
@@ -55,5 +114,37 @@ public class CouchDb : Database
             throw;
         }
         
+    }
+    
+    private static string ParseData(string[] data)
+    {
+        string parsedData = "";
+        foreach (var query in data)
+        {
+            parsedData += query+",\n";
+        }
+
+        parsedData = parsedData.Substring(0, parsedData.Length - 2);
+        string serializedData = "{ \"docs\" : [" + parsedData + "]}";
+        return serializedData;
+    }
+    
+    private static string GetAuthenticationStringInBase64(string[]? parameters)
+    {
+        if (parameters is null || parameters.Length < 1)
+        {
+            throw new Exception();
+        }
+        
+        if (parameters.Length != 2)
+        {
+            throw new Exception();
+        }
+        
+        var admin = parameters[0];
+        var password = parameters[1];
+        var authenticationString = $"{admin}:{password}";
+        var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(authenticationString));
+        return base64EncodedAuthenticationString;
     }
 }
